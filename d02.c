17ec46951f413f729e8ebd94ac330cee3d5a4b94
Daniel Babiak <d.babiak@gmail.com>
2019-04-24 20:57:24 -0700

    Day 2 Part 2

--- d02.c
+++ d02.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -25,6 +27,28 @@ bool has_char_with_k(int counts[], int k) {
   return false;
 }
 
+uint32_t common_chars(char *s1, char *s2, char *buf, size_t N) {
+  memset(buf, '\0', N);
+
+  uint32_t n = 0;
+
+  for (int i = 0; s1[i] && s2[i]; i++)
+    if (s1[i] == s2[i])
+      buf[n++] = s1[i];
+
+  return n;
+}
+
+char *part_two(Vec *lines) {
+  char buf[64];
+  for (int i = 0; i < lines->n - 1; i++)
+    for (int j = i + 1; j < lines->n; j++)
+      
+      if ((26/*✨🧙✨*/ - common_chars(V_get(lines, i), V_get(lines, j), buf, sizeof buf)) == 1)
+        return strdup(buf);
+  assert(false);
+}
+
 int main(int argc, char *argv[]) {
   Vec *lines  = IO_read_input(argv[1]);
 
@@ -43,4 +67,7 @@ int main(int argc, char *argv[]) {
   int d2_p1 = twos * threes;
 
   printf("part 1: %d\n", d2_p1);
+
+  char *d2_p2 = part_two(lines);
+  printf("part 2: %s\n", d2_p2);
 }
